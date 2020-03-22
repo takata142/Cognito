f6698ba9bbf45bc4f552e349e725af75f44eed0b
Yuto Iwata <jyuchan415@gmail.com>
2020-03-22 17:34:35 +0900

    SignUpViewController を追加.

--- CognitoSample/SignUpViewController.swift
+++ CognitoSample/SignUpViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SignInViewController.swift
+//  SignUpViewController.swift
 //  CognitoSample
 //
 //  Created by 岩田裕登 on 2020/03/22.
@@ -8,8 +8,8 @@
 
 import UIKit
 
-/// サインイン画面の ViewController.
-class SignInViewController: UIViewController {
+/// サインアップ画面の ViewController.
+class SignUpViewController: UIViewController {
     /// ユーザ名入力用 TextField.
     @IBOutlet weak var usernameField: UITextField!
     /// パスワード入力用 TextField.
@@ -17,13 +17,8 @@ class SignInViewController: UIViewController {
     /// 「サインイン」ボタン.
     @IBOutlet weak var signInButton: UIButton!
     
-    /// サインインする.
-    @IBAction func signIn(_ sender: UIButton) {
-    }
-    
-    /// 「新しいアカウントを作成する」ボタンを押した時の処理.
-    @IBAction func pushedSignUp(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "SignUp", sender: nil)
+    /// サインアップする.
+    @IBAction func signUp(_ sender: UIButton) {
     }
 }
 
