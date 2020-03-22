b64c17d34da5911a7b24593e4c7b6684eb443dd2
Yuto Iwata <jyuchan415@gmail.com>
2020-03-22 16:23:58 +0900

    Initial Commit

---
+++ CognitoSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CognitoSample
+//
+//  Created by 岩田裕登 on 2020/03/22.
+//  Copyright © 2020 Yuto Iwata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
