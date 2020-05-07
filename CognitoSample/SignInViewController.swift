1a67c1f69bac8dd7f0ec699234cb05de6056273b
Yuto Iwata <jyuchan415@gmail.com>
2020-05-07 16:28:32 +0900

    サインインボタン押下時のエラー表示とサインイン処理の一部を実装.

--- CognitoSample/SignInViewController.swift
+++ CognitoSample/SignInViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Yuto Iwata. All rights reserved.
 //
 
+import AWSCognitoIdentityProvider
 import UIKit
 
 /// サインイン画面の ViewController.
@@ -16,14 +17,37 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     /// 「サインイン」ボタン.
     @IBOutlet weak var signInButton: UIButton!
+    var passwordAuthenticationCompletion: AWSTaskCompletionSource<AWSCognitoIdentityPasswordAuthenticationDetails>?
     
     /// サインインする.
     @IBAction func signIn(_ sender: UIButton) {
+        guard let username: String = self.usernameField.text,
+            let password: String = self.passwordField.text else {
+                self.presentErrorAlert(title: "ユーザ名またはパスワードが入力されていません。", message: nil)
+                return
+        }
+        let authDetails: AWSCognitoIdentityPasswordAuthenticationDetails?
+            = AWSCognitoIdentityPasswordAuthenticationDetails(username: username, password: password)
+        self.passwordAuthenticationCompletion?.set(result: authDetails)
     }
     
     /// 「新しいアカウントを作成する」ボタンを押した時の処理.
     @IBAction func pushedSignUp(_ sender: UIButton) {
         self.performSegue(withIdentifier: "SignUp", sender: nil)
+    }
+    
+    /// エラーを示すアラートを表示する.
+    /// - Parameters:
+    ///   - title:   アラートのタイトル.
+    ///   - message: アラートのメッセージ.
+    func presentErrorAlert(title: String?, message: String?) {
+        /// エラーを表示するアラート.
+        let errorAlert: UIAlertController = UIAlertController(title: title,
+                                                              message: message,
+                                                              preferredStyle: .alert)
+        // アラートに「OK」ボタンを追加.
+        errorAlert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(errorAlert, animated: true)
     }
 }
 
