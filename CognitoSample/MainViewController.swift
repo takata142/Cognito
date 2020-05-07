8c06dd8c578768e7231aec87a9ef27d64c1e25c5
Yuto Iwata <jyuchan415@gmail.com>
2020-05-07 23:33:34 +0900

    サインインしているときは「サインイン」ボタンを隠す。

--- CognitoSample/MainViewController.swift
+++ CognitoSample/MainViewController.swift
@@ -6,14 +6,35 @@
 //  Copyright © 2020 Yuto Iwata. All rights reserved.
 //
 
+import AWSCognitoIdentityProvider
 import UIKit
 
 class MainViewController: UIViewController {
-
+    /// 「サインイン」ボタン.
+    @IBOutlet weak var signInButton: UIButton!
+    
     /// view がメモリにロードされた後に呼ばれる.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    /// view が表示される直前に呼ばれる. バックグラウンド復帰、タブ切り替えなどでも呼ばれる.
+    override func viewWillAppear(_ animated: Bool) {
+        let pool: AWSCognitoIdentityUserPool
+            = AWSCognitoIdentityUserPool(forKey: CognitoConstants.SignInProviderKey)
+        let user: AWSCognitoIdentityUser? = pool.currentUser()
+        user?.getSession().continueWith { (task) in
+            // セッションが切れている場合, サインアウトする.
+            if task.result == nil {
+                user?.signOut()
+            } else {
+                // サインインしているときは「サインイン」ボタンを無効化して隠す.
+                self.signInButton.isEnabled = false
+                self.signInButton.isHidden = true
+            }
+            return nil
+        }.waitUntilFinished()
     }
 
     /// 「サインイン」ボタンを押した時の処理.
