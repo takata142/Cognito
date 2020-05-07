2a28aa45cfbf754fdbea2648887e7de4a5701bde
Yuto Iwata <jyuchan415@gmail.com>
2020-05-07 16:36:14 +0900

    SignInViewController を AWSCognitoIdentityPasswordAuthentication に準拠.

--- CognitoSample/SignInViewController.swift
+++ CognitoSample/SignInViewController.swift
@@ -51,3 +51,20 @@ class SignInViewController: UIViewController {
     }
 }
 
+extension SignInViewController: AWSCognitoIdentityPasswordAuthentication {
+    
+    public func getDetails(_ authenticationInput: AWSCognitoIdentityPasswordAuthenticationInput, passwordAuthenticationCompletionSource: AWSTaskCompletionSource<AWSCognitoIdentityPasswordAuthenticationDetails>) {
+        self.passwordAuthenticationCompletion = passwordAuthenticationCompletionSource
+    }
+    
+    public func didCompleteStepWithError(_ error: Error?) {
+        DispatchQueue.main.async {
+            if let error = error as NSError? {
+                self.presentErrorAlert(title: error.userInfo["__type"] as? String,
+                                       message: error.userInfo["message"] as? String)
+            } else {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+}
