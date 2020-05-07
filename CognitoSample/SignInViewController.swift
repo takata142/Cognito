f0655e2f66ea3e17f2f252f6dc740046dd8f0399
Yuto Iwata <jyuchan415@gmail.com>
2020-05-07 17:08:37 +0900

    OceanData と同様のサインイン方法に変更.

--- CognitoSample/SignInViewController.swift
+++ CognitoSample/SignInViewController.swift
@@ -17,7 +17,6 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     /// 「サインイン」ボタン.
     @IBOutlet weak var signInButton: UIButton!
-    var passwordAuthenticationCompletion: AWSTaskCompletionSource<AWSCognitoIdentityPasswordAuthenticationDetails>?
     
     /// サインインする.
     @IBAction func signIn(_ sender: UIButton) {
@@ -26,9 +25,20 @@ class SignInViewController: UIViewController {
                 self.presentErrorAlert(title: "ユーザ名またはパスワードが入力されていません。", message: nil)
                 return
         }
-        let authDetails: AWSCognitoIdentityPasswordAuthenticationDetails?
-            = AWSCognitoIdentityPasswordAuthenticationDetails(username: username, password: password)
-        self.passwordAuthenticationCompletion?.set(result: authDetails)
+        print(username)
+        let pool: AWSCognitoIdentityUserPool = AWSCognitoIdentityUserPool(forKey: CognitoConstants.SignInProviderKey)
+        let user: AWSCognitoIdentityUser = pool.getUser(username)
+        user.getSession(username, password: password, validationData: nil).continueWith { task in
+            DispatchQueue.main.async {
+                if let error: NSError = task.error as NSError? {
+                    self.presentErrorAlert(title: error.userInfo["__type"] as? String,
+                    message: error.userInfo["message"] as? String)
+                } else {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+            return task
+        }
     }
     
     /// 「新しいアカウントを作成する」ボタンを押した時の処理.
@@ -48,23 +58,5 @@ class SignInViewController: UIViewController {
         // アラートに「OK」ボタンを追加.
         errorAlert.addAction(UIAlertAction(title: "OK", style: .default))
         self.present(errorAlert, animated: true)
-    }
-}
-
-extension SignInViewController: AWSCognitoIdentityPasswordAuthentication {
-    
-    public func getDetails(_ authenticationInput: AWSCognitoIdentityPasswordAuthenticationInput, passwordAuthenticationCompletionSource: AWSTaskCompletionSource<AWSCognitoIdentityPasswordAuthenticationDetails>) {
-        self.passwordAuthenticationCompletion = passwordAuthenticationCompletionSource
-    }
-    
-    public func didCompleteStepWithError(_ error: Error?) {
-        DispatchQueue.main.async {
-            if let error = error as NSError? {
-                self.presentErrorAlert(title: error.userInfo["__type"] as? String,
-                                       message: error.userInfo["message"] as? String)
-            } else {
-                self.dismiss(animated: true, completion: nil)
-            }
-        }
     }
 }
